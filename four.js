f12d044d0c2c68cba2ce16d8c66b518ba4be3823
Priyanka Mehta <primehta17@gmail.com>
2025-03-30 20:48:13 +0530

    third commit

---
+++ four.js
@@ -0,0 +1,9 @@
+//concatenation two array
+let arr1=[1,2,3];
+let arr2=[4,5,6];
+
+let arr3=[...arr1,...arr2];
+let arr4 = arr1.concat(arr2);
+
+console.log(arr3);
+console.log(arr4);
---
+++ three.js
@@ -0,0 +1,8 @@
+let arr = new Array();
+
+arr.push('a');
+arr.push('b');
+arr.pop();
+arr.push('v');
+
+console.log(arr);
